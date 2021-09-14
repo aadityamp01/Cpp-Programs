abeec0f02dd3f393878e99fc91b82c0d4e9fea4a
Aaditya Paithane <51539518+aadityamp01@users.noreply.github.com>
2021-09-14 23:16:44 +0530

    Problems added

---
+++ arrays/kadanesAlgo.cpp
@@ -0,0 +1,60 @@
+// Problem link
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Solution{
+    public:
+    // arr: input array
+    // n: size of array
+    //Function to find the sum of contiguous subarray with maximum sum.
+    int maxSubarraySum(int arr[], int n)
+    {
+        int overall_best = arr[0];
+        int curr_best = arr[0];
+        
+        for (int i = 1; i < n; i++)
+        {
+        curr_best = max(arr[i], curr_best+arr[i]);
+        overall_best = max(overall_best, curr_best);
+        }
+        return overall_best;
+    
+    }
+};
+
+
+int main()
+{
+    int t,n;
+    
+    cin>>t; //input testcases
+    while(t--) //while testcases exist
+    {
+        
+        cin>>n; //input size of array
+        
+        int a[n];
+        
+        for(int i=0;i<n;i++)
+            cin>>a[i]; //inputting elements of array
+            
+        Solution ob;
+        
+        cout << ob.maxSubarraySum(a, n) << endl;
+    }
+}
+
+/*
+Input:
+N = 5
+Arr[] = {1,2,3,-2,5}
+Output:
+9
+Explanation:
+Max subarray sum is 9
+of elements (1, 2, 3, -2, 5) which 
+is a contiguous subarray.
+*/
---
+++ arrays/kthsmallest.cpp
@@ -0,0 +1,42 @@
+//Problem link https://www.geeksforgeeks.org/kth-smallestlargest-element-unsorted-array-set-2-expected-linear-time/ 
+
+
+//Initial function template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution{
+    public:
+    int kthSmallest(int arr[], int l, int r, int k) {
+        sort(arr, arr+(r+1));
+        return arr[k-1];
+        
+        
+    }
+};
+
+int main()
+{
+    int test_case;
+    cin>>test_case;
+    while(test_case--)
+    {
+        int number_of_elements;
+        cin>>number_of_elements;
+        int a[number_of_elements];
+        
+        for(int i=0;i<number_of_elements;i++)
+            cin>>a[i];
+            
+        int k;
+        cin>>k;
+        Solution ob;
+        cout<<ob.kthSmallest(a, 0, number_of_elements-1, k)<<endl;
+    }
+    return 0;
+}  
+
+
+
+
